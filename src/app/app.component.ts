69434079e86948320e773efedb20ee5c25ea73b0
Kelly <kelly.liu@columbia.edu>
2017-10-25 18:03:54 -0700

    places api working in app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,94 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, NgZone, OnInit, AfterViewInit, ViewChild } from '@angular/core';
+declare var google: any;
+import { FormControl } from '@angular/forms';
+import { } from 'googlemaps';
+// import { AgmCoreModule, MapsAPILoader } from '@agm/core';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+
+export class AppComponent implements OnInit {
+  public latitude: number;
+  public longitude: number;
+  public searchControl: FormControl;
+  public zoom: number;
+  public google: any;
+  public map: any;
+  public service: any;
+  public infoWindow: any;
+
+  public results: any[];
+  
+  // @ViewChild("search")
+  // public searchElementRef: ElementRef;
+
+  // constructor(
+  //   private mapsAPILoader: MapsAPILoader,
+  //   private ngZone: NgZone
+  // ) {}
+
+  ngOnInit () {
+    console.log('this is ', this);
+    const initCoords = { lat: 37.7758, lng: -122.435 }; // this is SF
+    const mapOptions = {
+      center: initCoords, 
+      zoom: 13
+    };
+
+    this.map = new google.maps.Map(document.getElementById('map'), mapOptions);
+  
+    this.service = new google.maps.places.PlacesService(this.map);
+    this.service.textSearch({
+      location: initCoords,
+      radius: 500,
+      query: 'restaurant'
+    }, this.callback.bind(this));
+
+    
+  
+    // The idle event is a debounced event, so we can query & listen without
+    // throwing too many requests at the server.
+    // this.map.addListener('idle', this.callback);
+  }
+
+  callback(results, status) {
+    console.log(results);
+    if (status == google.maps.places.PlacesServiceStatus.OK) {
+      for (var i = 0; i < results.length; i++) {
+        var place = results[i];
+        console.log('place', place);
+        
+        this.createMarker(place);
+      }
+    }
+  }
+
+  createMarker(place) {
+    console.log('place', place);
+    var placeLoc = place.geometry.location;
+    var marker = new google.maps.Marker({
+      map: this.map,
+      position: place.geometry.location
+    });
+
+    google.maps.event.addListener(marker, 'click', function() {
+      this.infowindow.setContent(place.name);
+      this.infowindow.open(this.map, this);
+    });
+  }
+
+
+  private setCurrentPosition() {
+    if ("geolocation" in navigator) {
+      navigator.geolocation.getCurrentPosition((position) => {
+        this.latitude = position.coords.latitude;
+        this.longitude = position.coords.longitude;
+        this.zoom = 12;
+      });
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, ApplicationRef } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 import { AgmCoreModule } from '@agm/core';
 
 import { AppComponent } from './app.component';
